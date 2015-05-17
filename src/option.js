c062a89be7b15bfc47f673768554cb3915bd3f08
Igor Stepanov <sigorilla@gmail.com>
2015-05-17 16:02:14 +0300

    Change infrastructure and LF

---
+++ src/option.js
@@ -0,0 +1,16 @@
+chrome.storage.sync.get({
+	remember: false
+}, function (item) {
+	document.querySelector("#remember").checked = item.remember;
+});
+
+document.querySelector("#remember").onchange = function () {
+	chrome.storage.sync.set({
+		remember: this.checked
+	}, function () {
+		if (chrome.extension.lastError) {
+			console.log("Error: %o", chrome.extension.lastError.message);
+		}
+	});
+	document.querySelector("#callback").innerText = "Saved!";
+};
